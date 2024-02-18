fd414114292175aa2a0dff455452caf4a84c2bdd
syedfarrukh0 <ssyedfarrukh0@gmail.com>
2024-02-18 05:05:44 +0500

    push

--- script_our_portfolio.js
+++ script_our_portfolio.js
@@ -6,10 +6,143 @@ setTimeout(() => {
     document.querySelector('.main-logo').src = '/assets/header-logo/orgitech_logo.png';
     document.querySelector('.main-logo').alt = 'orgitech_logo';
 
-    document.querySelector('.popUpImg').srcset ='';
+    document.querySelector('.popUpImg').srcset ='/assets/icons/form-pen.gif';
     document.querySelector('.popUpImg').src ='/assets/icons/form-pen.gif';
 
-    document.querySelector('.footer-dark-img').srcset ='';
+    document.querySelector('.footer-dark-img').srcset = ''
     document.querySelector('.footer-dark-img').src ='/assets/footer-main/Vector.png';
 
-}, 1000);
+    var project_img_1 = '_next/1.webp';
+    var project_img_2 = '_next/2.jpg';
+    var project_img_3 = '_next/3.jpg';
+    var project_img_4 = '_next/4.jpg';
+    var project_img_5 = '_next/5.jpg';
+    var project_img_6 = '_next/6.jpg';
+    var project_img_7 = '_next/7.jpg';
+    var project_img_8 = '_next/8.jpg';
+    var project_img_9 = '_next/9.jpg';
+    var project_img_10 = '_next/10.jpg';
+
+    var a = document.querySelectorAll('.Double_stretchyWrapper__12RJ8 img'); 
+    a[0].srcset = '';
+    a[0].src = project_img_1;
+
+    a[1].srcset = '';
+    a[1].src = project_img_2;
+
+    a[2].srcset = '';
+    a[2].src = project_img_3;
+
+    a[3].srcset = '';
+    a[3].src = project_img_4;
+
+    a[4].srcset = '';
+    a[4].src = project_img_5;
+
+    a[5].srcset = '';
+    a[5].src = project_img_6;
+
+    a[6].srcset = '';
+    a[6].src = project_img_7;
+
+    a[7].srcset = '';
+    a[7].src = project_img_8;
+
+    a[8].srcset = '';
+    a[8].src = project_img_9;
+
+    a[9].srcset = '';
+    a[9].src = project_img_10;
+
+    
+    // //////////////////
+    // slider images
+    // /////////////////
+    
+    const slider = document.querySelectorAll('.parallax-slider img');
+    var slide_img_1 = '/assets/slider/1.jpg';
+    var slide_img_2 = '/assets/slider/2.jpg';
+    var slide_img_3 = '/assets/slider/3.jpg';
+    var slide_img_4 = '/assets/slider/4.jpg';
+    var slide_img_5 = '/assets/slider/5.jpg';
+    var slide_img_6 = '/assets/slider/6.jpg';
+    var slide_img_7 = '/assets/slider/7.jpg';
+    var slide_img_8 = '/assets/slider/8.jpg';
+    var slide_img_9 = '/assets/slider/9.jpg';
+    var slide_img_10 = '/assets/slider/10.jpg';
+    var slide_img_11 = '/assets/slider/11.jpg';
+    var slide_img_12 = '/assets/slider/12.jpg';
+    var slide_img_13 = '/assets/slider/13.jpg';
+    var slide_img_14 = '/assets/slider/14.jpg';
+    var slide_img_15 = '/assets/slider/15.jpg';
+    var slide_img_16 = '/assets/slider/16.jpg';
+    var slide_img_17 = '/assets/slider/17.jpg';
+    var slide_img_18 = '/assets/slider/18.jpg';
+    var slide_img_19 = '/assets/slider/19.jpg';
+    var slide_img_20 = '/assets/slider/20.jpg';
+    var slide_img_21 = '/assets/slider/21.jpg';
+
+    slider[0].srcset='';
+    slider[0].src= slide_img_1;
+
+    slider[1].srcset='';
+    slider[1].src= slide_img_2;
+
+    slider[2].srcset='';
+    slider[2].src= slide_img_3;
+
+    slider[3].srcset='';
+    slider[3].src= slide_img_4;
+    
+    slider[4].srcset='';
+    slider[4].src= slide_img_5;
+
+    slider[5].srcset='';
+    slider[5].src= slide_img_6;
+
+    slider[6].srcset='';
+    slider[6].src= slide_img_7;
+
+    slider[7].srcset='';
+    slider[7].src= slide_img_8;
+
+    slider[8].srcset='';
+    slider[8].src= slide_img_9;
+
+    slider[9].srcset='';
+    slider[9].src= slide_img_10;
+
+    slider[10].srcset='';
+    slider[10].src= slide_img_11;
+
+    slider[11].srcset='';
+    slider[11].src= slide_img_12;
+
+    slider[12].srcset='';
+    slider[12].src= slide_img_13;
+
+    slider[13].srcset='';
+    slider[13].src= slide_img_14;
+
+    slider[14].srcset='';
+    slider[14].src= slide_img_15;
+
+    slider[15].srcset='';
+    slider[15].src= slide_img_16;
+
+    slider[16].srcset='';
+    slider[16].src= slide_img_17;
+
+    slider[17].srcset='';
+    slider[17].src= slide_img_18;
+
+    slider[18].srcset='';
+    slider[18].src= slide_img_19;
+
+    slider[19].srcset='';
+    slider[19].src= slide_img_20;
+
+    slider[20].srcset='';
+    slider[20].src= slide_img_21;
+
+}, 5000);
